902f003dac1ed650c15907d9bfabdaee662f366a
Stephen Sylvia <ssylvia@esri.com>
2013-03-06 14:07:57 -0500

    begin body html and load webmap

--- app/core/core.js
+++ app/core/core.js
@@ -17,6 +17,44 @@ define(["esri/map",
 
 		function init()
 		{
+			app = {
+				//esri Map
+				map: null
+			}
+
+			// Set the Portal
+			// - if configOptions.sharingurl is set use that URL
+			// - if app is accessed through localhost : use ArcGIS Online
+			// - use the server name and port of the 
+			if (!configOptions.sharingurl) {
+				if( location.host.match("localhost") )
+					configOptions.sharingurl = "http://www.arcgis.com/sharing/rest/content/items";
+				else
+					configOptions.sharingurl = location.protocol + '//' + location.host + "/sharing/content/items";
+			}
+
+			if (configOptions.geometryserviceurl && location.protocol === "https:")
+				configOptions.geometryserviceurl = configOptions.geometryserviceurl.replace('http:', 'https:');
+
+			configOptions.proxyurl = configOptions.proxyurl || location.protocol + '//' + location.host + "/sharing/proxy";
+
+			esri.arcgis.utils.arcgisUrl = configOptions.sharingurl;
+			esri.config.defaults.io.proxyUrl = configOptions.proxyurl;
+			esri.config.defaults.geometryService = new esri.tasks.GeometryService(configOptions.geometryserviceurl);
+
+			loadMap();
+
+		}
+
+		function loadMap()
+		{
+			var mapDeferred = esri.arcgis.utils.createMap(configOptions.webmap,"map",{
+				sliderStyle: "small"
+			});
+			
+			mapDeferred.then(function(response){
+				map = response.map;
+			});
 		}
 
 		return {
--- config.js
+++ config.js
@@ -1,5 +1,30 @@
-var configOptions = {
-	webmap: "9629c5f58dc5418788bb96cb9c82321d",
-	title: "",
-	subtitle: ""
-}
+define([],
+	function ()
+	{
+		function init()
+		{
+			configOptions = {
+				//The appid for the configured application
+				appid: "",
+				//The web map id
+				webmap: "66502e241a6e4f72a1d9c1a80fcbc2d7",
+				//Enter a title, if no title is specified, the webmap's title is used.
+				title: "",
+				//Enter a subtitle, if not specified the ArcGIS.com web map's summary is used
+				subtitle: "",
+				//If the webmap uses Bing Maps data, you will need to provided your Bing Maps Key
+				bingmapskey: "Akt3ZoeZ089qyG3zWQZSWpwV3r864AHStal7Aon21-Fyxwq_KdydAH32LTwhieA8",
+				// Specify a proxy for custom deployment
+				proxyurl: "",
+				//specify the url to a geometry service
+				geometryserviceurl: "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer",
+				//Modify this to point to your sharing service URL if you are using the portal
+				sharingurl: ""
+			}
+		}
+
+		return {
+			init: init
+		}
+	}
+);
