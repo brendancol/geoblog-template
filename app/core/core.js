56b38bd7302e8ddd7ff4e6dc75f117a47d8e17b0
Stephen Sylvia <ssylvia@esri.com>
2013-03-06 11:04:51 -0500

    initial source setup

---
+++ app/core/core.js
@@ -0,0 +1,27 @@
+define(["esri/map",
+		"esri/arcgis/utils",
+		"esri/layout",
+		"esri/widgets"],
+	function(Map,Utils,Layout,Widgets)
+	{
+		/**
+		 * Core
+		 * @class Core
+		 *
+		 * Geoblog viewer Main class
+		 */
+
+		//
+		// Initialization
+		//
+
+		function init()
+		{
+		}
+
+		return {
+			init: init
+		};
+
+	}
+);
---
+++ config.js
@@ -0,0 +1,5 @@
+var configOptions = {
+	webmap: "9629c5f58dc5418788bb96cb9c82321d",
+	title: "",
+	subtitle: ""
+}
